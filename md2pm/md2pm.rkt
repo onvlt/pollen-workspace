95e51adaf113cb577251b3150d63ccfe7c67ebcb
Ondřej Nývlt <ondrej@nyv.lt>
2024-09-15 22:46:23 +0200

    WIP

--- md2pm/md2pm.rkt
+++ md2pm/md2pm.rkt
@@ -12,10 +12,7 @@
                 (format "#:~a ~a" key value)) attrs)))
 
 
-(define (wrap-content-with-newlines? expr)
-  (and (block-txexpr? expr) (not (member (get-tag expr) '(h1 h2 h3 h4 h5 h6 li hr)))))
-
-(define (encode-tag expr)
+(define (encode-tag expr #:wrap-content-with-newlines? [wrap-content-with-newlines? #f])
   (define attrs (get-attrs expr))
   (define elements (get-elements expr))
   (define tag-str (format "◊~a" (get-tag expr)))
@@ -23,20 +20,24 @@
     (cond [(empty? attrs) #f]
           [else (format "[~a]" (encode-attrs attrs))]))
   (define elements-str
-    (cond [(wrap-content-with-newlines? expr)
-           (format "{\n~a\n}" (apply string-append elements))]
+    (cond [wrap-content-with-newlines? (format "{\n~a\n}" (apply string-append elements))]
           [else (format "{~a}" (apply string-append elements))]))
 
   (apply string-append (filter identity (list tag-str attrs-str elements-str))))
 
 (define/contract (encode-txexpr expr)
   (-> txexpr? any/c)
-  (define tag (get-tag expr))
+  (define-values (tag attrs elements) (values (get-tag expr) (get-attrs expr) (get-elements expr)))
   (cond
     [(regexp-match? #rx"^temp-" (symbol->string tag)) expr]
-    [(eq? tag 'root) (apply string-append (get-elements expr))]
-    ((eq? tag 'p) (apply string-append (get-elements expr)))
-    [(member tag '(ul ol)) (encode-tag expr)]
+    [(eq? tag 'root) (apply string-append elements)]
+    ((eq? tag 'p) (apply string-append elements))
+    ((eq? tag 'h1) (encode-tag (txexpr 'chapter attrs elements)))
+    ((eq? tag 'h2) (encode-tag (txexpr 'section attrs elements)))
+    ((eq? tag 'h3) (encode-tag (txexpr 'subsection attrs elements)))
+    ((eq? tag 'ul) (encode-tag (txexpr 'bullet-list attrs elements) #:wrap-content-with-newlines? #t))
+    ((eq? tag 'ol) (encode-tag (txexpr 'number-list attrs elements) #:wrap-content-with-newlines? #t))
+    ((eq? tag 'li) (apply string-append (cons "◊(item) " elements)))
     [#t (encode-tag expr)]))
 
 (define (intersperse-newlines elements)
